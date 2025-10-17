c21ccc708759f98cb6a189c0cb0daf33f323e96b
Apham130 <andypham4747@gmail.com>
2025-10-17 18:17:06 +0000

    in class problem done

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    int i = 3;
+    while (i <=25)
+    {
+      System.out.println(i);
+      i += 2;
+    }
+
+
+
   }
 }
